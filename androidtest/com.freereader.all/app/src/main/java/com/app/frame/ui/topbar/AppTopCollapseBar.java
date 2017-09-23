a8788b089f0123848c7865944334dd4008912a8f
kanxue <1151182524@qq.com>
2017-09-23 17:29:36 +0800

    添加了topbar 新文件

---
+++ androidtest/com.freereader.all/app/src/main/java/com/app/frame/ui/topbar/AppTopCollapseBar.java
@@ -0,0 +1,12 @@
+package com.app.frame.ui.topbar;
+
+/**
+ * Created by Administrator on 2017/9/23.
+ */
+public class AppTopCollapseBar {
+//    （2）动态加入layout 的功能。
+//            （3）Appbar 可以收缩功能。
+//            （4）共享组件的模式。
+//            （4）夜间模式：这里是设置ThemeManager 更新界面。
+    // 动画模式。
+}
